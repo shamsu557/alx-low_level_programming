e4297ac509c79f43b070c5f00d8251ec8c1bc9ab
shamsu557 <1440shamsusabo@gmail.com>
2022-12-10 23:09:45 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
-#include <stdio.h>
+#include <unistd.h>
 /**
- * main-print and that piece of art ..
- * Return:1
+ * main - Entry point
+ *
+ * Return:0 (Success)
  */
 int main(void)
 {
-	char Ch[]
-		= "and that piece of art is useful\"-Dora Korpar, 2015-10-19\n";
-	write(2, ch, sizeof(ch));
-	return (1);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
+return (1);
 }
