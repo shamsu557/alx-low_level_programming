e396bb250c9f0ca6b2f7c3307a605870610521ba
shamsu557 <1440shamsusabo@gmail.com>
2022-12-10 21:48:00 -0800

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,17 +1,20 @@
 #include <stdio.h>
+#include <ctype.h>
+
 /**
- * main-progarm to get upper lower case letters from A to Z
- * Return:alwaays 0 (Success)
+ * main - program that prints the alphabet in lowercase, followed by a new line
+ * You can only use the putchar
+ * Return: 0
  */
+
 int main(void)
 {
-	char c;
-
-	for (c = 'a', c = 'z', c = c++)
-		putchar(c, '\n');
-
-	for (c = 'A', c = 'Z', c = c++)
-		putchar(c, '\n');
-			Return 0;
+int l = 'a';
+while (l <= 'z')
+{
+putchar(l);
+l += 1;
 }
-
+putchar('\n');
+return (0);
+}
