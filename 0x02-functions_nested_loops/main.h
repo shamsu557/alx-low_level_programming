b0312700e2323027697f452dfa8e33173acbf6d2
shamsu557 <1440shamsusabo@gmail.com>
2022-12-14 08:40:24 -0800

    correction for main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,18 @@
-#ifndef MAIN_H
-#define MAIN_H
 
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+#ifndef MAIN_IF_INCLUDED
+#define MAIN_IF_INCLUDED
+
+int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -12,7 +24,9 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
-int _putchar(char c)
+void print_times_table(int n);
+
+
 
 #endif
 
