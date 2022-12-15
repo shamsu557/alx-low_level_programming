0347e4ae62552b66fb78baf3a682a5d09c7b025a
shamsu557 <1440shamsusabo@gmail.com>
2022-12-14 18:23:35 -0800

    main.h

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_IF_INCLUDED
+#define MAIN_IF_INCLUDED
+
+#include <stdio.h>
+
+void positive_or_negative(int i);
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+int convert_day(int month, int day);
+#endif
