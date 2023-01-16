7c4393de0f589d786f0e2bb273a73677bfdf8dab
shamsu557 <1440shamsusabo@gmail.com>
2023-01-16 11:34:22 -0800

    2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,4 +1,7 @@
-#ifndef absolute_value
-#define absolute_value
-#define ABS(x) (((x) < 0) ? (x) * (-1) : (x))
-#endif
+#include <stdio.h>
+
+int main() {
+    printf("%s\n", __FILE__);
+    return 0;
+}
+
