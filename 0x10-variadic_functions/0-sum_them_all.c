10487766a14700b69974903efbae031fb4990f4e
shamsu557 <1440shamsusabo@gmail.com>
2023-01-19 21:29:15 -0800

    0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <stdarg.h>
 #include "variadic_functions.h"
 /**
  * sum_them_all - the sum of all its parameters
@@ -28,3 +28,4 @@ int sum_them_all(const unsigned int n, ...)
 	va_end(parameters);
 	return (add);
 }
+
