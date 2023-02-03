29e157b91fcf617dc1008568f5a501def9c1e912
shamsu557 <1440shamsusabo@gmail.com>
2023-02-02 19:00:34 -0800

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,19 +10,16 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int num = 0;
-	int len = 0;
-
-	if (b[len] == '\0')
-		return (0);
-
-	while ((b[len] == '0') || (b[len] == '1'))
+	int i; 
+	unsigned int dec_val=0;
+	if (!b)
+		return 0;
+          for  (i=0; b[i];i++)
 	{
-		num <<= 1;
-		num += b[len] - '0';
-		len++;
+		if (b[i]< '0' || b[i]>'1')
+			return 0;
+		dec_val = 2*dec_val +(b[i]-'0');
 	}
-
-	return (num);
+		return dec_val;
 }
 
