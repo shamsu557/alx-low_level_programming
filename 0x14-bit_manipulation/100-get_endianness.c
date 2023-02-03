1fe0ce059d33f392afbd267921e6f7655f316192
shamsu557 <1440shamsusabo@gmail.com>
2023-02-02 19:11:23 -0800

    100-get_endianness.c

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -6,19 +6,13 @@
  * Return: If big-endian - 0.
  *         If little-endian - 1.
  */
-int get_endianness(void)
-{
-	int n;
 
-    n = get_endianness();
-    if (n != 0)
-    {
-        printf("Little Endian\n");
-    }
-    else
-    {
-        printf("Big Endian\n");
-    }
-    return (0);
+int get_endianness(void) {
+    union {
+        uint32_t i;
+        char c[4];
+    } endian_test = {0x01020304};
+    return endian_test.c[0] == 1;
 }
+
 
